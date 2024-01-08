04ff612b7be71669dfb00208b16ee99fea6c5308
ZayyTheWizard <isaiah.washington19@yahoo.com>
2024-01-08 04:02:15 -0500

    added predict function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import joblib
 
 def train():
     # Data Preprocessing 
@@ -34,10 +35,37 @@ def train():
     ann.fit(X_Train, Y_Train, batch_size=32, epochs=50000)
 
     ann.save('TrainedSPY')
+    joblib.dump(sc_x, 'sc_x_scaler.save')
+    joblib.dump(sc_y, 'sc_y_scaler.save')
 
     pred = ann.predict(sc_x.transform([[468.30, 470.96, 467.05, 84230214]]))
     print(sc_y.inverse_transform(pred))
 
+def predict(open: float, high: float, low: float, vol: int):
+    sc_x = joblib.load('sc_x_scaler.save')
+    sc_y = joblib.load('sc_y_scaler.save')
+    loaded_ann = tf.keras.models.load_model('TrainedSPY')
+
+    input_data = np.array([[open, high, low, vol]])
+    pred = loaded_ann.predict(sc_x.transform(input_data))
+
+    return sc_y.inverse_transform(pred)
 
 if __name__ == '__main__':
-    train()
+    while(True):
+        open_price: float = input("open price?: \n")
+        high_price: float = input("high price?: \n")
+        low_price: float = input("low price?: \n")
+        vol: int = input("open vol?: \n")
+
+        prediction = predict(open_price, low_price, high_price, vol)
+        print(prediction)
+
+        choice = input("Would you like to continue y/n: ")
+
+        if choice == 'y':
+            continue
+        elif choice == 'n':
+            break
+        elif choice != 'y' or choice != 'n':
+            break
